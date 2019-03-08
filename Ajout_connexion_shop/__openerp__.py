7d4cb746b5b73c85326e666874b8f7781bba84db
Dalila <34835331+dalilahannouche@users.noreply.github.com>
2019-03-08 15:05:20 +0100

    Update __openerp__.py

--- Ajout_connexion_shop/__openerp__.py
+++ Ajout_connexion_shop/__openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Login / Signup Popup in Shop page',
     'category': 'Website',
     'summary': 'The user can register and login in Shop page',
-    'website': 'https://www.linkedin.com/in/dalila-hannouche-8a6896143/',
+    'website': 'https://www.linkedin.com/in/dalila-h-8a6896143/',
     'repository' : 'https://github.com/dalilahannouche/Ajout_connexion_shop.git',
     'version': '1.0',
     'description': """
